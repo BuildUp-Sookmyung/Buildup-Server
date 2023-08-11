38b950cd9a6315252ffbaa0458f11a0ea689031d
NamJihyun99 <namjihyun9936@gmail.com>
2023-08-11 22:19:57 +0900

    test:> member join test/#2

--- src/test/java/buildup/server/member/service/MemberServiceTest.java
+++ src/test/java/buildup/server/member/service/MemberServiceTest.java
@@ -43,13 +43,14 @@ class MemberServiceTest extends DummyObject {
         ArrayList<String> interests = new ArrayList<>();
         interests.add("연구/개발");
         interests.add("디자인");
+        ProfileSaveRequest profileSaveRequest = new ProfileSaveRequest("jojo",
+                "username@naver.com",
+                "Sookmyung Women's University",
+                "Computer Science", "4", "N", interests);
         LocalJoinRequest request = new LocalJoinRequest(
                 "username",
                 passwordEncoder.encode("password4321"),
-                new ProfileSaveRequest("jojo",
-                        "username@naver.com",
-                        "Sookmyung Women's Universitiy",
-                        "Computer Science", "4", "N", interests),
+                profileSaveRequest,
                 "Y"
         );
         Member member = newMember(request);
@@ -58,7 +59,7 @@ class MemberServiceTest extends DummyObject {
         Mockito.when(memberRepository.findAllByEmailAndUsername(ArgumentMatchers.any(), ArgumentMatchers.any()))
                 .thenReturn(new ArrayList<>());
         Mockito.when(memberRepository.save(ArgumentMatchers.any())).thenReturn(member);
-        Mockito.when(profileService.saveProfile(ArgumentMatchers.any(), ArgumentMatchers.any())).thenReturn(profileEntity.getId());
+        Mockito.when(profileService.saveProfile(profileSaveRequest, member)).thenReturn(profileEntity.getId());
         Mockito.when(authService.createAuth(ArgumentMatchers.any())).thenReturn(tokenProvider.createAuthToken(member.getUsername(), new Date(new Date().getTime()+1800000)));
         Mockito.when(authService.setRefreshToken(ArgumentMatchers.any())).thenReturn(new MemberRefreshToken(member.getUsername(), "refreshToken"));
 
